2c045e574eea95516f05158ab406e9bc6848a744
nataliag409 <natalia212329@gmail.com>
2024-05-05 19:52:27 -0700

    chjgj

--- Unit5P5Gutierrez/Assets/Scripts/Target.cs
+++ Unit5P5Gutierrez/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
